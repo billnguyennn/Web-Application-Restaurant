421c021dafe1152c535f5b69fd9e89b459383ed2
TeeRexNg <lnguyen1410@gmail.com>
2022-05-05 11:00:09 -0400

    add sign up page

---
+++ frontend/src/component/Pages/User.js
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import { env } from '../../env';
+import Link from '@mui/material/Link';
+
+const theme = createTheme();
+
+function SignInUser() {
+    let navigate = useNavigate();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // eslint-disable-next-line no-console
+        //query db
+        const signIn = await axios.post(env.API_HOST + '/user', {
+            username: data.get('username'),
+            password: data.get('password'),
+        });
+
+        //this if statement to check if we have a correct Admin username & password
+        if (signIn.data === true) {
+            localStorage.setItem("logInStatusUser", true);
+            return navigate("/");
+        } else {
+            alert("Wrong password");
+        }
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="username"
+                            name="username"
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Link href="/SignUp" variant="body2">
+                            {"Don't have an account? Sign Up"}
+                        </Link>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+}
+
+export default SignInUser;
